1146b69816d7a2ab9d1ec51ee35d5db724075dc9
Esteban-maker66 <e306711@gmail.com>
2026-08-22 15:51:35 -0400

    FEAT: time optimized for avoid bing telemetry

--- rewards_bot.py
+++ rewards_bot.py
@@ -43,6 +43,8 @@ HEADLESS_MODE = os.getenv("HEADLESS", "true").lower() in ("true", "1", "yes")
 
 ROJO = "\033[31m"
 VERDE = "\033[32m"
+AMARILLO = "\033[33m"
+AZUL = "\033[34m"
 RESET = "\033[0m"
 
 def hideFootPrintBot(context):
@@ -53,9 +55,10 @@ def hideFootPrintBot(context):
         });
     """)
 
+
 def launchEdgeContext(pw, device_conf):
     os.makedirs(PROFILE_PATH, exist_ok=True)
-    print(f"[i] Edge user data dir: {PROFILE_PATH}", flush=True)
+    print(f"{AZUL}[i] Edge user data dir: {PROFILE_PATH}{RESET}", flush=True)
 
     context = pw.chromium.launch_persistent_context(
         user_data_dir=PROFILE_PATH,
@@ -72,6 +75,7 @@ def launchEdgeContext(pw, device_conf):
     hideFootPrintBot(context)
     return context
 
+
 def cleanupProfileLock():
   """Removes stale Singleton locks before launching Edge to avoid ProcessSingleton errors."""
   lock_files = [
@@ -86,21 +90,20 @@ def cleanupProfileLock():
         os.remove(lock_file)
       except OSError:
         pass
-
+      
 
 def typeSearch(page, keyword):
     """Types keyword into search input & submit query."""
     search_select = "textarea[name='q'], input[name='q'], #sb_form_q"
 
     try:
-        page.wait_for_selector(search_select, timeout=3000)
+        page.wait_for_selector(search_select, timeout=4000)
         search_input = page.locator(search_select).first
 
-        search_input.click()
         search_input.fill("")
 
-        search_input.type(keyword, delay=random.randint(45, 90))
-        time.sleep(random.uniform(0.25, 0.4))
+        search_input.type(keyword, delay=random.randint(80, 100))
+        time.sleep(random.uniform(0.5, 0.7))
 
         search_input.press("Enter")
         page.wait_for_load_state("domcontentloaded")
@@ -141,29 +144,30 @@ KEYWORDS = loadKeywords()
 
 
 def execMobileSearch():
-    print("\n(+) Starting Bing Rewards Bot (A16 5G Mobile mode)...")
+    print(f"\n{VERDE}(+) Starting Bing Rewards Bot (A16 5G Mobile mode)...{RESET}")
 
     with sync_playwright() as pw:
         context = launchEdgeContext(pw, SAMSUNG_A16_5G_CONF)
         page = activePage(context)
 
         try:
-            print("[x] Connecting to Microsoft Bing...")
+            print(f"{AZUL}[...] Connecting to Microsoft Bing.\n{RESET}")
             page.goto("https://www.bing.com", wait_until="domcontentloaded")
-            time.sleep(random.uniform(2.05, 2.95))
+            time.sleep(random.uniform(3.85, 5.95))
 
             random.shuffle(KEYWORDS)
-            MOBILEDAILYSEARCH = KEYWORDS[:20]
+            MOBILEDAILYSEARCH = KEYWORDS[:10]
 
             for idx, keyword in enumerate(MOBILEDAILYSEARCH, 1):
-                print(f"-- [{idx}/{len(MOBILEDAILYSEARCH)}] Searching:\n'{keyword}...'")
+                print(f"• ({idx}/{len(MOBILEDAILYSEARCH)}) Searching:\n'{keyword}...'")
 
                 typeSearch(page, keyword)
 
-                wait_time = random.uniform(2.66, 3.66)
+                wait_time = random.uniform(4.35, 6.05)
                 time.sleep(wait_time)
 
             print(f"\n{VERDE}[✓] Searching cycle has been completed succesfully.{RESET}")
+            time.sleep(1)
 
         except Exception as e:
             print(f"\n{ROJO}[ER]: Error during Mobile bot execution at: {e}{RESET}")
@@ -173,30 +177,31 @@ def execMobileSearch():
 
 
 def execDesktopSearch():
-    print("\n(+) Starting Bing Rewards Bot (Desktop Mode)...")
+    print(f"\n{VERDE}(+) Starting Bing Rewards Bot (Desktop Mode)...{RESET}")
 
     with sync_playwright() as pw:
         context = launchEdgeContext(pw, DESKTOP_CONF)
         page = activePage(context)
 
         try:
-            print("[x] Connecting to Microsoft Bing...")
+            print(f"{AZUL}[...] Connecting to Microsoft Bing.{RESET}\n")
             page.goto("https://www.bing.com", wait_until="domcontentloaded")
-            time.sleep(random.uniform(2.05, 2.95))
+            time.sleep(random.uniform(3.85, 5.95))
 
             random.shuffle(KEYWORDS)
-            DAILYSEARCH = KEYWORDS[:31]
+            DAILYSEARCH = KEYWORDS[:10]
 
             for idx, keyword in enumerate(DAILYSEARCH, 1):
-                print(f"-- [{idx}/{len(DAILYSEARCH)}] Searching (Desktop):\n'{keyword}...'")
+                print(f"• ({idx}/{len(DAILYSEARCH)}) Searching (Desktop):\n'{keyword}...'")
 
                 typeSearch(page, keyword)
 
-                wait_time = random.uniform(2.66, 3.66)
+                wait_time = random.uniform(4.35, 6.05)
                 time.sleep(wait_time)
                 
 
             print(f"\n{VERDE}[✓] Desktop searching cycle has been completed successfully.{RESET}")
+            time.sleep(1)
             return True
 
         except Exception as e:
@@ -211,7 +216,7 @@ if __name__ == "__main__":
     if not execDesktopSearch():
         raise SystemExit(1)
 
-    print("\n[$] Intermission: Profile switching in 2 seconds...")
+    print(f"\n{AMARILLO}[$] Intermission: Profile switching in 2 seconds...{RESET}")
     time.sleep(2)
 
     if not execMobileSearch():
